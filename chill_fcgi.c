3a6bec4d4202b100843884e25cbcffc11c920168
kimsos0723 <kimsos0723@gmail.com>
2020-11-25 16:41:08 +0900

    google coding st

--- chill_fcgi.c
+++ chill_fcgi.c
@@ -2,23 +2,25 @@
 #include <stdlib.h>
 
 int main(void) {
-	while (FCGI_Accept() >= 0) {
-		printf("Status: 200 OK\r\n");
-		printf("Set-Cookie: a=aaa;\r\n");		
-		printf("Content-type: text/html\r\n\r\n");
-		char* cookie = getenv("HTTP_COOKIE");
-		if (cookie == NULL) {
-			cookie = "NULL";
-		}
-		
-		printf("<!doctype>"
-		       "<html>"
-		       "<body>"
-		       "from fastcgi with lighttpd! <br>"
-			   "set cookie %s<br>"
-			   "%s<br>"
-		       "</body>"
-		       "</html>\n", cookie, getenv("QUERY_STRING"));
-	}
-	return EXIT_SUCCESS;
+  while (FCGI_Accept() >= 0) {
+    printf("Status: 200 OK\r\n");
+    printf("Set-Cookie: a=aaa;\r\n");
+    printf("Content-type: text/html\r\n\r\n");
+    char* cookie = getenv("HTTP_COOKIE");
+    if (cookie == NULL) {
+      cookie = "NULL";
+    }
+
+    printf(
+        "<!doctype>"
+        "<html>"
+        "<body>"
+        "from fastcgi with lighttpd! <br>"
+        "set cookie %s<br>"
+        "%s<br>"
+        "</body>"
+        "</html>\n",
+        cookie, getenv("QUERY_STRING"));
+  }
+  return EXIT_SUCCESS;
 }
--- decorator_test.c
+++ decorator_test.c
@@ -5,47 +5,44 @@
 typedef struct chill_pointer chill_string;
 typedef struct chill_blueprint chill_blueprint;
 
-#define CONCAT_(x,y) x##y
-#define GEN_FUNC_NAME(x,y) CONCAT_(x,y)
-#define CHILL_ROUTE_FUNC_NAME_BASE CHILLXX_ROUTE_FUNC_BASE_ // if you use function or value name start with this, You make sure the names overlap
-#define ADD_BRUEPRINT(__url, __options, __fn_name) \
-    extern chill_string __fn_name(chill_string options);\
-    void __attribute__((constructor)) GEN_FUNC_NAME(CHILL_ROUTE_FUNC_NAME_BASE, __COUNTER__ ) () {\
-        __fn_name(make_chill_string(#__options));\
-    }\
-    chill_string __fn_name(chill_string options)
+#define CONCAT_(x, y) x##y
+#define GEN_FUNC_NAME(x, y) CONCAT_(x, y)
+#define CHILL_ROUTE_FUNC_NAME_BASE \
+  CHILLXX_ROUTE_FUNC_BASE_  // if you use function or value name start with
+                            // this, You make sure the names overlap
+#define ADD_BRUEPRINT(__url, __options, __fn_name)               \
+  extern chill_string __fn_name(chill_string options);           \
+  void __attribute__((constructor))                              \
+      GEN_FUNC_NAME(CHILL_ROUTE_FUNC_NAME_BASE, __COUNTER__)() { \
+    __fn_name(make_chill_string(#__options));                    \
+  }                                                              \
+  chill_string __fn_name(chill_string options)
 
 struct chill_pointer {
-    char* str;
-    size_t len;
+  char* str;
+  size_t len;
 };
 
 struct chill_blueprint {
-    chill_string url;
-    chill_string options;    
+  chill_string url;
+  chill_string options;
 };
 
 extern chill_string make_chill_string(char* str);
 
 ADD_BRUEPRINT("/", foo1, foo) {
-    printf("%s\n", options.str);
-    return make_chill_string("bar");
+  printf("%s\n", options.str);
+  return make_chill_string("bar");
 }
 
 ADD_BRUEPRINT("/", bar2, bar) {
-    printf("%s\n", options.str);
-    return make_chill_string("bar");
+  printf("%s\n", options.str);
+  return make_chill_string("bar");
 }
 
-int main()
-{
-    
-    return 0;
-}
+int main() { return 0; }
 
 chill_string make_chill_string(char* str) {
-    chill_string a = { str, strlen(str) };    
-    return a;
+  chill_string a = {str, strlen(str)};
+  return a;
 }
-
-
